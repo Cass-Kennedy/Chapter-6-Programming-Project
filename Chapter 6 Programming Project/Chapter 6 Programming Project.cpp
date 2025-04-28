54ef673376b57d0e336b6b54bd6b2e30e8c370a2
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-28 15:46:27 -0400

    Did everything but comments and error validation. Breaks if you have a space in the region names, but the fix for that also breaks the style.

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -1,14 +1,62 @@
 #include <iostream>
 using namespace std;
 
-void regionInfo(string a, int b);
+void regionInfo(string &a, int &b);
 
 bool isLower(int a, int b);
 
-void showLowest(string a);
+void showLowest(string a, int b);
 
 int main()
 {
-    return 0;
+int userEntry, firstAccident, newAccident, saferAccident;
+userEntry = 0;
+string firstRegion, newRegion, saferRegion;
+cout << "Please enter the name of a region in your area: ";
+cin >> firstRegion;
+cout << "Please enter how many car crashes occured there last year ";
+cin >> firstAccident;
+cout << "Please enter another region's name ";
+cin >> newRegion;
+cout << "Please enter how many car crashes occured there last year ";
+cin >> newAccident;
+if (isLower(firstAccident, newAccident) || firstAccident == newAccident) {
+    saferAccident = newAccident;
+    saferRegion = newRegion;
+};
+if (isLower(newAccident, firstAccident)) {
+    saferAccident = firstAccident;
+    saferRegion = firstRegion;
+};
+cout << "\nWould you like to enter another region's information? 0 for no, 1 for yes. ";
+cin >> userEntry;
+while (userEntry != 0) {
+    cout << "Please enter the name of a regions in your area ";
+    cin >> newRegion;
+    cout << "Please enter how many car crashes occured there last year ";
+    cin >> newAccident;
+    if (isLower(saferAccident, newAccident)) {
+        saferRegion = newRegion;
+        saferAccident = newAccident;
+    };
+    cout << "\nWould you like to enter another region's information? 0 for no, 1 for yes. ";
+    cin >> userEntry;
+};
+showLowest(saferRegion, saferAccident);
+return 0;
 }
 
+void regionInfo(string& a, int& b) {
+    cout << "Please enter the name of the region: ";
+    cin >> a;
+    cout << "Please enter the number of accidents: ";
+    cin >> b;
+};
+
+bool isLower(int a, int b) {
+    return a > b;
+};
+
+void showLowest(string a, int b) {
+    cout << a << " is the saftest region to drive because it has the lowest crash rate with " << b << " crashes last year.";
+};
