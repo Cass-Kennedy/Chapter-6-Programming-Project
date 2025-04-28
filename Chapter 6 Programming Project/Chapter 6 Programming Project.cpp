81de51548df2e6b53bb81ceca3b294d4994e0766
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-28 14:17:21 -0400

    Skeleton

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -1,39 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void getLength_Width(int &a, int &b);
+void regionInfo(string a, int b);
 
-int calcPerimeter(int a, int b);
+bool isLower(int a, int b);
 
-int calcArea(int a, int b);
-
-void displayResults(int a, int b);
-
+void showLowest(string a);
 
 int main()
 {
-    int sideOne{}, sideTwo{};
-    getLength_Width(sideOne,sideTwo);
-    displayResults(calcPerimeter(sideOne,sideTwo), calcArea(sideOne,sideTwo));
     return 0;
 }
 
-void getLength_Width(int &a, int &b) {
-    cout << "Please enter the length and width of the rectangle: ";
-    cin >> a;
-    cin >> b;
-};
-
-int calcPerimeter(int a, int b) {
-    return (2 * a) + (2 * b);
-    
-};
-
-int calcArea(int a, int b) {
-    return (a * b);
-};
-
-void displayResults(int a, int b) {
-    cout << "The perimeter of the rectangle is " << a << "\n";
-    cout << "The area of the rectangle is " << b << endl;
-};
