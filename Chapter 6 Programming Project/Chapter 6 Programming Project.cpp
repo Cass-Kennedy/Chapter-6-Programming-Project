eecc45625f4bb5a675c6f29af71bea601a2f7ec0
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-29 18:08:13 -0400

    Fixed using integer division.

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 void getFarenheit(int &a, int &b);
 
-double calcCelsius(int a);
+double calcCelsius(double a);
 
 void displayResults(int a, int b);
+int addition(int a, int b);
 
 
 int main()
 {
-    calcCelsius(212);
-    /*int farenheitInitial, farenheitIncrements, celsiusInitial;
+    int farenheitInitial, farenheitIncrements, celsiusInitial;
     cout << "This program makes a table that converts farenheit temperatures to celcius." << endl;
     getFarenheit(farenheitInitial,farenheitIncrements);
     while (farenheitInitial <= 20) {
         celsiusInitial = calcCelsius(farenheitInitial);
         displayResults(farenheitInitial, celsiusInitial);
         farenheitInitial += farenheitIncrements;
-    };*/
+    };
     return 0;
 }
 
@@ -29,11 +29,15 @@ void getFarenheit(int &a, int &b) {
     cin >> b;
 };
 
-double calcCelsius(int a) {
-    return ((5 / 9) * (a - 32));
+double calcCelsius(double a) {
+    return (5.0/9.0)*(a-32);
 };
 
 void displayResults(int a, int b) {
     cout << "Farenheit: " << a;
     cout << setw(14) << "Celsius: " << b << endl;
+};
+
+int addition(int a, int b) {
+    return a + b;
 };
