02d7a77de11375ec94b43c4227f701a004232c32
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-29 11:38:54 -0400

    Need to do a bit more testing, but this mostly works.

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
 using namespace std;
 
-void getScore(int &a, int &b);
+void getScore(double &a);
 
-bool isLower(int a, int b);
+bool isLower(double a, double b);
 
-bool isHigher(int a, int b);
+bool isHigher(double a, double b);
 
 double calcAverage(double a, int b, double c, double d);
 
 
 int main()
 {
-    int sideOne{}, sideTwo{};
-    getScore(sideOne,sideTwo);
+    double userEntry, userScore, minScore, maxScore, cumulativeScore;
+    int counter;
+    counter = 1;
+    getScore(userScore);
+    minScore = userScore;
+    maxScore = userScore;
+    cumulativeScore = userScore;
+    do {
+        cout << "Please enter your score: ";
+        cin >> userScore;
+        if (isLower(userScore, minScore)) {
+            minScore = userScore;
+        };
+        if (isHigher(userScore, maxScore)) {
+            maxScore = userScore;
+        };
+        cumulativeScore += userScore;
+        counter += 1;
+        cout << "Would you like to enter another score? 0 for no, 1 for yes. ";
+        cin >> userEntry;
+    } while (userEntry != 0);
+    if (counter >= 3) {
+        cout << "Your score is " << calcAverage(cumulativeScore, counter, minScore, maxScore);
+    }
+    else {
+        cout << "Not enough scores to calculate correctly.";
+    };
     return 0;
 }
 
-void getScore(int &a, int &b) {
-    cout << "Please enter the length and width of the rectangle: ";
+void getScore(double &a) {
+    cout << "Please enter your first score: ";
     cin >> a;
-    cin >> b;
 };
 
-bool isLower (int a, int b) {
+bool isLower (double a, double b) {
     return a<b; //int b = min score
     
 };
 
-bool isHigher (int a, int b) {
+bool isHigher (double a, double b) {
     return a>b; //int b = max score
 };
 
