ed639bb772408b1b6c086e4926d613cb455002fa
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-30 12:26:51 -0400

    Added some comments.

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void regionInfo(string &a, int &b);
+void regionInfo(string &a, int &b); //Function prototypes.
 
 bool isLower(int a, int b);
 
@@ -14,16 +14,10 @@ int main()
 int userEntry, firstAccident, newAccident, saferAccident;
 userEntry = 0;
 string firstRegion, newRegion, saferRegion;
-cout << "Please enter the name of a region in your area: ";
-cin >> firstRegion;
-cout << "Please enter how many car crashes occured there last year: ";
-cin >> firstAccident;
+regionInfo(firstRegion, firstAccident); //Gets the starting information
 inputValidation(firstAccident);
-cout << "Please enter another region's name ";
-cin >> newRegion;
-cout << "Please enter how many car crashes occured there last year ";
-cin >> newAccident;
-if (isLower(firstAccident, newAccident) || firstAccident == newAccident) {
+regionInfo(newRegion, newAccident); //Gets the second set of information.
+if (isLower(firstAccident, newAccident) || firstAccident == newAccident) { //These two if statements set the first safest statment.
     saferAccident = newAccident;
     saferRegion = newRegion;
 };
@@ -31,41 +25,41 @@ if (isLower(newAccident, firstAccident)) {
     saferAccident = firstAccident;
     saferRegion = firstRegion;
 };
-cout << "\nWould you like to enter another region's information? 0 for no, 1 for yes. ";
+cout << "\nWould you like to enter another region's information? 0 for no, 1 for yes. "; //Meant to allow the user to enter however many regions as they want.
 cin >> userEntry;
 while (userEntry != 0) {
-    cout << "Please enter the name of a region in your area ";
-    cin >> newRegion;
-    cout << "Please enter how many car crashes occured there last year ";
-    cin >> newAccident;
-    inputValidation(newAccident);
-    if (isLower(saferAccident, newAccident)) {
+    regionInfo(newRegion, newAccident);
+    inputValidation(newAccident); //Makes sure the accident number is positive.
+    if (isLower(saferAccident, newAccident)) { //Determines if the new region is safer.
         saferRegion = newRegion;
         saferAccident = newAccident;
     };
     cout << "\nWould you like to enter another region's information? 0 for no, 1 for yes. ";
     cin >> userEntry;
 };
-showLowest(saferRegion, saferAccident);
+showLowest(saferRegion, saferAccident); //Displays the overall results.
 return 0;
 }
 
-void regionInfo(string& a, int& b) {
-    cout << "Please enter the name of the region: ";
+void regionInfo(string& a, int& b) { //Gets the region name from the user.
+    static int counter;
+    counter = 1;
+    cout << "Please enter the name of the region " << counter << ": ";
     cin >> a;
+    counter++;
     cout << "Please enter the number of accidents: ";
     cin >> b;
 };
 
-bool isLower(int a, int b) {
+bool isLower(int a, int b) { //Compares two accident rates.
     return a > b;
 };
 
-void showLowest(string a, int b) {
+void showLowest(string a, int b) { //Displays the overall results.
     cout << a << " is the saftest region to drive because it has the lowest crash rate with " << b << " crashes last year.";
 };
 
-void inputValidation(int a) {
+void inputValidation(int a) { //Makes sure the input is acceptable.
     while (a < 0) {
         cout << "Please enter a positive accident number. ";
         cin >> a;
