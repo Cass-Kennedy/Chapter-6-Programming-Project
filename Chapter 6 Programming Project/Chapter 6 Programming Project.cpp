9661a6a5b8e1a1739f19218f4df7797d4414beaa
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-30 13:13:29 -0400

    Cooments.

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -1,9 +1,9 @@
+//Calculates the price and parts of a paint job.
 #include <iostream>
-#include <iomanip>
 #include <cmath>
 using namespace std;
 
-void totalSquareFootage(double a, double &b);
+void totalSquareFootage(double a, double &b); //Function prototypes.
 
 int scalingConstant(double a);
 
@@ -16,62 +16,63 @@ void finalReport(int a, double b, double c);
 int main()
 {
     int roomNumber, counter, scaleNumber;
-    counter = 0;
+    counter = 0; //Tracks how many rooms the user wants to paint.
     double inputWallSpace{}, cumulativeWallSpace{}, paintCost, totalPaintCost, laborCost;
-    laborCost = 25.0;
-    paintCost = 0.0;
+    laborCost = 25.0; //Given labor price.
+    paintCost = 0.0; //Initial paint cost, the user will input another value later.
     cout << "How many rooms do you want painted? ";
-    cin >> roomNumber;
-    while (roomNumber < 1) {
-        cerr << "Please input a valid number of rooms. ";
+    cin >> roomNumber; //How many times to run the square foot loop.
+    while (roomNumber < 1) { //Input validation.
+        cerr << "Please input a valid number of rooms. "; //Error message
         cin >> roomNumber;
     };
-    while (counter != roomNumber) {
-        totalSquareFootage(inputWallSpace, cumulativeWallSpace);
-        counter += 1;
+    while (counter != roomNumber) { //This loops gets total square footage.
+        totalSquareFootage(inputWallSpace, cumulativeWallSpace); //Gets the square footage in each room and adds them together.
+        counter += 1; //Controls the loop.
     };
-    if (cumulativeWallSpace <= 0) {
+    if (cumulativeWallSpace <= 0) { //Input validation.
         cout << "Please enter a valid square footage.";
     }
-    else {
-        scaleNumber = scalingConstant(cumulativeWallSpace);
-        while (paintCost < 10) {
-            totalPaintCost = paintInformation(paintCost, scaleNumber);
+    else { 
+        scaleNumber = scalingConstant(cumulativeWallSpace); //Makes a number based off of the amount of wall space.
+        while (paintCost < 10) { //Controls the paint cost.
+            totalPaintCost = paintInformation(paintCost, scaleNumber); //Gets the total cost of the paint.
             if (paintCost < 10) {
-                cerr << "Please input a valid paint cost (>$10). ";
+                cerr << "Please input a valid paint cost (>$10). "; //Error message.
             }
         }
-        laborHours(scaleNumber, laborCost);
-        finalReport(scaleNumber, totalPaintCost, laborCost);
+        laborHours(scaleNumber, laborCost); //Gets the total cost of the labor.
+        finalReport(scaleNumber, totalPaintCost, laborCost); //Displays the final results.
     }
     return 0;
 }
 
-void totalSquareFootage(double a, double &b) {
+void totalSquareFootage(double a, double &b) { //Gets the square footage in each room. The second parameter is to make a total count.
     static int count;
-    count++;
+    count++; //Displays the room number.
     cout << "How many square feet of wall space in room " << count << "? ";
     cin >> a;
     b += a;
 };
 
-int scalingConstant(double a) {
+int scalingConstant(double a) { //Makes a number for calculating prices based off of the square footage.
     return ceil(a / 110.0);
 };
 
-double paintInformation(double &a, int b) {
+double paintInformation(double &a, int b) { //Gets the price of paint and returns the price of it.
     cout << "What is the price of your prefered paint? ";
     cin >> a;
     return a*b;
 };
 
-void laborHours(int a, double &b) {
+void laborHours(int a, double &b) { //Calculates the amount and cost of labor.
     int laborPer = 8;
     double costPer = 25.0;
     b = a * costPer * laborPer;
 };
 
-void finalReport(int a, double b, double c) { // a will be scalingConstant, b will be paintInfo, c will be labor hours
+void finalReport(int a, double b, double c) { //Meant to give a full report of prices and time.
+    // a will be scalingConstant, b will be paintInfo, c will be labor hours
     int laborPer = 8;
     cout << "The amount of paint required is " << a << " gallons.\n";
     cout << "The total paint cost is $" << b << ".\n";
